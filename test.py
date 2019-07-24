9de2ee345c585621cd9a7b6d5fb3b22b550d1ddd
Durgesh <durgeshreddiyar@yahoo.com>
2019-07-24 11:54:28 +0530

    tensor from given data

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 from __future__ import print_function
 import torch
 
-x = torch.empty(5, 3)
+x = torch.tensor([5.5,3])
 print(x)
-
-y = torch.rand(5, 3)
-print(y)
-
-z = torch.zeros(5, 3, dtype=torch.long)
-print(z)
