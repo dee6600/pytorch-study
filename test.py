4c8289ea3137fec1455f68fb852f48acb371f7ee
Durgesh <durgeshreddiyar@yahoo.com>
2019-07-25 12:21:05 +0530

    added autograd  notebook

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 from __future__ import print_function
 import torch
 
-x = torch.tensor([5.5,3])
+x = torch.rand(5,3, dtype= torch.double)
 print(x)
 print(x.size())
 
-x = x.new_ones(5, 3, dtype=torch.double)
-print(x,x.size())
+y = torch.rand(5, 3, dtype=torch.double)
+print(y,y.size())
+
+print(x + y)
+
+print(y.add_(x))
