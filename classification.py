e70b54a557088e4bec99eaf92a8d018dfd6558f4
Durgesh <durgeshreddiyar@yahoo.com>
2019-07-27 11:18:48 +0530

    added classifier notebook with data of 10 categories

---
+++ classification.py
@@ -0,0 +1,17 @@
+#%env CUDA_VISIBLE_DEVICES=2
+
+import torch, torchvision
+from torchvision import datasets, models, transforms
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import time
+from torchsummary import summary
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from PIL import Image
+
+print("hello world")
